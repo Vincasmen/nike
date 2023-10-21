0cc7e698c0c6ba43b11ef3ec9fe48c47d018a26e
Vincasmen <vincasmen13@gmail.com>
2023-10-21 11:38:08 +0200

    Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,29 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col 
+      justify-center min-h-screen gap-10 max-container"
+    >
+      <div
+        className="relative xl:w-2/5 flex 
+      flex-col justify-center items-start w-full 
+      max-xl:padding-x pt-28"
+      >
+        <p>Aout Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span>
+          shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active live.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
