15c69f76f40e68ce5f28ade4e00cf573d2894051
Vincasmen <vincasmen13@gmail.com>
2023-10-26 23:24:22 +0200

    Progress on the page

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,38 @@
+import Button from "../components/Button";
+
+import { shoe8 } from "../assets/images";
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col 
+      gap- 10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize  font-bold lg:max-w-lg">
+          We Provide You
+          <span className="text-coral-red"> Super </span>
+          <span className="text-coral-red"> Quality </span>
+          shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality quality, innovation and touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedictaion to detail and excellence ensure your satisfaction.
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img src={shoe8} alt="shoe8" width={570} height={522} />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
